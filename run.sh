68236cdc5d8d4e4f14290208b07ce61cd52df44b
rithika-ganesan <152159409+rithika-ganesan@users.noreply.github.com>
2024-01-31 16:51:09 +0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
-filename="pp_qcd.txt"
-massfilename="mass_lq.txt"
-pdg=4210012
+# This script generates a text file containing MadGraph5 commands to loop over different masses for a particle.
+
+filename="pp_qcd.txt" #Change what the output text file is called
+massfilename="mass.txt" #Add text file containing masses, similarly decay widths 
+pdg=4210012 #PDG code for the particle whose mass is being varied
 
 iter_number=$(wc -l < $massfilename)
 rm $filename
 
-echo -e "import model U1_noCKM_UFO/  
-define pb = p b b~ a 
-define u11x = u11 u11~
+#Add your initialization commands here
+echo -e "define pb = p b b~ a 
 define jb = j j b 
-generate pb pb > u11x u11x QCD==2 QED==0 VLQ==0 
-output k0_u1_pairproduction" >> "$filename"
+generate pb pb > jb jb QCD==2 QED==0 
+output output_name" >> "$filename"
 
 for (( i = 1; i<=$iter_number; i++ ))
 do
 
-imass=`head -$i $massfilename | tail -1 | awk {'print $1'}`
+imass=`head -$i $massfilename | tail -1 | awk {'print $1'}` #Pulls data from the mass text file
 echo -e "launch -n run_mass$imass
 set param_card yuku1ll 3 3 1 
 set param_card mass $pdg $imass" >> "$filename"
