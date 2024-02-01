927e90102311891f6a7116017749094809ac0ae5
rithika-ganesan <152159409+rithika-ganesan@users.noreply.github.com>
2024-02-01 13:15:41 +0400

    Update get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#run like: bash get_data.sh <cmd file name.txt> <nohup out container file.txt> <data output file.txt>  
+
 # Check if three arguments are provided
 if [ "$#" -ne 3 ]; then
     echo "Usage: $0 <filename> <nohupfilename> <outputfilename>"
